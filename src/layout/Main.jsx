48f22b83d53e22fc38ea811703bb2eab8810eb6b
Leonid <leonidchernobylskiy@gmail.com>
2021-02-16 23:00:57 +0200

    header+footer+main

---
+++ src/layout/Main.jsx
@@ -0,0 +1,7 @@
+function Main () {
+    return(
+        <main className="container content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi amet ex, beatae neque suscipit rem odio quis asperiores maxime, laborum cum deleniti reiciendis veniam culpa, officia provident? Perferendis, velit eaque?</main>
+    );
+}
+
+export {Main};
