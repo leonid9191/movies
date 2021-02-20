5dc288381c83a1a918b63a1dce83a94a30f087c3
Leonid <leonidchernobylskiy@gmail.com>
2021-02-20 15:12:56 +0200

    validation of search

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,12 +1,15 @@
 import {Movie} from './Movie'
 
 function Movies (props) {
-    const {movies} = props;
+    //get array with movies by default it's empty
+    const {movies = []} = props;
 
+    //if array still empty we see "Nothing found"
     return <div className="movies">
-        {movies.map(movie => (
+        {movies.length ? movies.map(movie => (
             <Movie key={movie.imdbID} {...movie}/>
-        ))}
+        )) : (<h4>Nothing found</h4>)
+    }
     </div>
 }
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import {Movies} from '../components/Movies'
-import {Preloader} from '../components/Preloader'
-import {Search} from '../components/Search'
+import React from 'react';
+import { Movies } from '../components/Movies';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 class Main extends React.Component {
     state = {
         movies: [],
-    }
+        loading: true,
+    };
 
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=755ceb25&s=cube')
-        .then(response => response.json())
-        .then(data => this.setState({movies: data.Search}))
+        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=cube`)
+            .then((response) => response.json())
+            .then((data) =>
+                this.setState({ movies: data.Search, loading: false })
+            );
     }
 
     searchMovies = (str, type = 'all') => {
-        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=755ceb25&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
-        .then(response => response.json())
-        .then(data => this.setState({movies: data.Search}))
-    }
-    render(){
-        const {movies} = this.state;
-        return <main className="container content">
-            <Search searchMovies={this.searchMovies}/>
-            {
-                movies.length ? (
-                    <Movies movies={this.state.movies}/>
-                ) : <Preloader />
-
-            }
-                </main>
+        this.setState({ loading: true });
+        fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`
+        )
+            .then((response) => response.json())
+            .then((data) =>
+                this.setState({ movies: data.Search, loading: false })
+            );
+    };
+    // if loading == true we see Preloader another we see all cards from API
+    render() {
+        const { movies, loading } = this.state;
+        return (
+            <main className="container content">
+                <Search searchMovies={this.searchMovies} />
+                {loading ? <Preloader /> : <Movies movies={movies} />}
+            </main>
+        );
     }
 }
 
-export {Main};
+export { Main };
